a217d58d709b0d96c7add20891d5705e5788868f
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2017-04-08 23:20:56 -0400

    Spring boot Startup files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # My First App with Spring Boot
 
 Topics covered
->Spring Boot starters
->Containers Deployments
->Auto-configuration
->Testing with Spring Boot
+*Spring Boot starters
+*Containers Deployments
+*Auto-configuration
+*Testing with Spring Boot
---
+++ boot-demo/src/main/java/com/rama/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.rama.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "Rama Boot, My First boot Controller";
+	}
+}
