a36bc926a72d04f6bd5a6d6059c92d24b39cd8e5
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2017-04-26 23:26:29 -0400

    web integration test added

---
+++ boot-demo/src/test/java/com/rama/ShipwreckRepositoryIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.rama;
+
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rama.model.Shipwreck;
+import com.rama.repository.ShipwreckRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(App.class)
+public class ShipwreckRepositoryIntegrationTest {
+
+	@Autowired
+	private ShipwreckRepository shipwreckRepository;
+	@Test
+	public void testFindAll() {
+		List<Shipwreck> wrecks = shipwreckRepository.findAll();
+		assertThat(wrecks.size(), is(greaterThanOrEqualTo(0)));
+	}
+}
